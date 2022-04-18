1eb3f59bf3c35236c9920a9c846d1933cf9553cf
dexpiper <dexpiper@yandex.ru>
2022-04-18 16:34:21 +0300

    Building basic structure

--- memc_load.go
+++ memc_load.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
-	log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus" // logging
 )
 
-func set_logging(settings map[string]string) {
+func processLog(device_memc map[string]string, pattern string, dry bool) {
+	// main logic
+	results := map[string]int{
+		"processed": 0,
+		"errors":    0,
+	}
+	log.Info("Processing")
+	log.Info(
+		fmt.Sprintf(
+			"Total processed: %d; Total errors: %d",
+			results["processed"],
+			results["errors"],
+		))
+	log.Info("Exiting")
+}
+
+func setLogging(settings map[string]string) {
 
 	// settings log output
 	logfile := settings["logfile"]
@@ -33,18 +50,49 @@ func set_logging(settings map[string]string) {
 	}
 }
 
+func getDefaultPattern() string {
+	default_dict := os.Getenv("default_dict")
+	if default_dict == "" {
+		default_dict = "."
+	}
+	default_pattern := fmt.Sprintf("%s/*.tsv.gz", default_dict)
+	return default_pattern
+}
+
 func main() {
+
 	logfile := flag.String("logfile", "stdout", "Logfile name")
 	loglevel := flag.String("loglevel", "info", "For debug level use debug")
+	idfa := flag.String("idfa", "127.0.0.1:33013", "idfa address")
+	gaid := flag.String("gaid", "127.0.0.1:33014", "gaid address")
+	adid := flag.String("adid", "127.0.0.1:33015", "adid address")
+	dvid := flag.String("dvid", "127.0.0.1:33016", "dvid address")
+	pattern := flag.String("pattern", getDefaultPattern(), "example: <dir>/*.tsv.gz")
+	dry := flag.Bool("dry", false, "turn in dryrun (without actual memcaching)")
 	flag.Parse()
-	settings := make(map[string]string)
-	settings["logfile"] = *logfile
-	settings["loglevel"] = *loglevel
-	set_logging(settings)
+
+	logset := make(map[string]string)
+	logset["logfile"] = *logfile
+	logset["loglevel"] = *loglevel
+	setLogging(logset)
+
+	device_memc := make(map[string]string)
+	device_memc["idfa"] = *idfa
+	device_memc["gaid"] = *gaid
+	device_memc["adid"] = *adid
+	device_memc["dvid"] = *dvid
+
 	log.WithFields(log.Fields{
 		"loglevel": *loglevel,
 		"logfile":  *logfile,
+		"idfa":     *idfa,
+		"gaid":     *gaid,
+		"adid":     *adid,
+		"dvid":     *dvid,
+		"dry":      *dry,
+		"pattern":  *pattern,
 	}).Info("Starting the application")
-	log.Debug("Something noteworthy happened")
-	log.Error("Something went wrong")
+
+	processLog(device_memc, *pattern, *dry)
+
 }
